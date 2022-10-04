cf32ff6ddadf090d34871cbc557fd6026985f60d
dvanca <vanca37@gmail.com>
2022-10-04 12:15:21 +0300

    first commit

---
+++ src/Classroom.java
@@ -0,0 +1,94 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Classroom {
+
+    private List<StudentGrade> grades;
+
+    public Classroom(List<StudentGrade> grades){
+            this.grades = new ArrayList<>(grades);
+
+    }
+
+
+   public List<Integer> getGradesForDiscipline(String discipline) {
+      List<Integer> gradesList = new ArrayList<>();
+      for (StudentGrade student : grades) {
+         if (student.getDiscipline().equals(discipline)) {
+           gradesList.add(student.getGrade());
+            }
+        }
+        return gradesList;
+    }
+
+    public List<Integer> getGradesForStudent(String student){
+        List<Integer> studentGradesList = new ArrayList<>();
+        for(StudentGrade students : grades){
+            if(students.getName().equals(student)){
+                studentGradesList.add(students.getGrade());
+            }
+        }
+        return studentGradesList;
+    }
+
+    public StudentGrade getMaxGrade(String discipline){
+        StudentGrade studentMaxGrade = null;
+        int maxgrade = 1;
+        for(StudentGrade student : grades){
+            if( student.getDiscipline().equals(discipline) && student.getGrade()> maxgrade){
+                maxgrade = student.getGrade();
+                studentMaxGrade = student;
+            }
+        }
+        return studentMaxGrade;
+    }
+
+    public StudentGrade getMaxGrade(){
+        StudentGrade studentWithMaxGrade = null;
+        int maxgrade = 1;
+        for(StudentGrade student : grades){
+            if(student.getGrade()>maxgrade){
+                maxgrade = student.getGrade();
+                studentWithMaxGrade = student;
+            }
+        }
+        return studentWithMaxGrade;
+    }
+
+    public Double getAverageGrade(String discipline){
+        double averagegrade=0;
+        int i=0;
+        for(StudentGrade student : grades){
+            if(student.getDiscipline().equals(discipline)){
+                i=i+1;
+                averagegrade = averagegrade + student.getGrade();
+            }
+        }
+        averagegrade = averagegrade / i;
+        return averagegrade;
+    }
+
+    public StudentGrade getWorstGrade(String discipline){
+        StudentGrade worstGradeStudent = null;
+        int worstGrade=100;
+        for(StudentGrade student : grades){
+            if(student.getDiscipline().equals(discipline) && worstGrade > student.getGrade()){
+                worstGrade = student.getGrade();
+                worstGradeStudent = student;
+            }
+        }
+        return worstGradeStudent;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Classroom{" +
+                "grades=" + grades +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("files", "grades.txt"));
+        List<StudentGrade> grades = new ArrayList<>();
+        for(String line : lines){
+            grades.add(readStudentGradeFromLine(line));
+        }
+        System.out.println(grades);
+
+        Classroom classroom = new Classroom(grades);
+        System.out.println(classroom.getGradesForDiscipline("Computer Science"));
+        System.out.println(classroom.getGradesForStudent("Gasparo Sava"));
+        System.out.println(classroom.getMaxGrade("Mathematics"));
+        System.out.println(classroom.getMaxGrade());
+        System.out.println(classroom.getAverageGrade("Mathematics"));
+        System.out.println(classroom.getWorstGrade("Computer Science"));
+    }
+    private static StudentGrade readStudentGradeFromLine(String line) {
+        String[] components = line.split("\\|");
+        return new StudentGrade(components[0], components[1], Integer.parseInt(components[2]));
+    }
+}
